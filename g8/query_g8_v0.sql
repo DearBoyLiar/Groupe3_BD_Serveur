0e78f89421e82b4dff8daf8a83cf25491fe29fe2
enzomartineau <enzo.martineau@free.fr>
2018-01-10 14:51:53 +0100

    no message

--- g8/query_g8_v0.sql
+++ g8/query_g8_v0.sql
@@ -9,3 +9,15 @@ WHERE s.date_publication BETWEEN CURRENT_DATE-7 AND CURRENT_DATE;
 SELECT s.id_word, s.tf_idf
 FROM statistique s
 WHERE s.date_publication BETWEEN CURRENT_DATE-30 AND CURRENT_DATE;
+
+SELECT count(s.id_word)
+FROM statistique s
+WHERE s.date_publication = CURRENT_DATE;
+
+SELECT count(s.id_word)
+FROM statistique s
+WHERE s.date_publication BETWEEN CURRENT_DATE-7 AND CURRENT_DATE;
+
+SELECT count(s.id_word)
+FROM statistique s
+WHERE s.date_publication BETWEEN CURRENT_DATE-30 AND CURRENT_DATE;
